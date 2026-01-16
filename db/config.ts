bb92199de07598079831c49197c4dfa5f23c7069
shuhib <shuhib_s@live.de>
2026-01-16 12:55:24 +0100

    todo_a1: setup db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from "astro:db";
 
-// https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Todo_a1: defineTable({
+      columns: {
+        id: column.number({
+          primaryKey: true,
+          autoIncrement: true,
+          unique: true,
+        }),
+        title: column.text(),
+        completed: column.boolean({ default: false }),
+        priority: column.number({ default: 0 }),
+        created_at: column.date({ default: NOW }),
+        updated_at: column.date({ default: NOW, onUpdate: NOW }),
+      },
+    }),
+  },
 });
